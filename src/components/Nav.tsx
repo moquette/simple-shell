badc5b734b040756db610a674cd515d5438b322c
Joaquin A. Moquette <joaquin@moquette.us>
2022-04-24 17:38:22 -0700

    footer icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react'
 import {NavLink} from 'react-router-dom'
 import {Popover} from '@headlessui/react'
-
-const pages = [
-  {title: `Home`, href: `/`},
-  {title: `Resume`, href: `/resume`},
-  {title: `Portfolio`, href: `/portfolio`},
-]
+import {
+  RiHome2Line,
+  RiQuillPenLine,
+  RiLayoutTop2Line,
+  RiLinkedinLine,
+  RiGithubLine,
+  RiMailLine,
+} from 'react-icons/ri'
 
 export default function Nav() {
   return (
@@ -26,16 +28,26 @@ export default function Nav() {
           <Popover.Panel className="popover-panel">
             <div className="wrapper">
               <div>
-                {pages.map((item) => (
-                  <Popover.Button
-                    as={NavLink}
-                    to={item.href}
-                    key={item.title}
-                    className="navbar-link"
-                  >
-                    {item.title}
-                  </Popover.Button>
-                ))}
+                <Popover.Button as={NavLink} to="/" className="navbar-link">
+                  <RiHome2Line className="inline-block ml-2 mr-5 text-3xl" />
+                  Home
+                </Popover.Button>
+                <Popover.Button
+                  as={NavLink}
+                  to="/resume"
+                  className="navbar-link"
+                >
+                  <RiQuillPenLine className="inline-block ml-2 mr-5 text-3xl" />
+                  Resume
+                </Popover.Button>
+                <Popover.Button
+                  as={NavLink}
+                  to="/portfolio"
+                  className="navbar-link"
+                >
+                  <RiLayoutTop2Line className="inline-block ml-2 mr-5 text-3xl" />
+                  Portfolio
+                </Popover.Button>
                 <hr />
                 <a
                   href="https://github.com/moquette/"
@@ -43,6 +55,7 @@ export default function Nav() {
                   className="navbar-link"
                   rel="noreferrer"
                 >
+                  <RiGithubLine className="inline-block ml-2 mr-5 text-3xl" />
                   GitHub
                 </a>
                 <a
@@ -51,6 +64,7 @@ export default function Nav() {
                   className="navbar-link"
                   rel="noreferrer"
                 >
+                  <RiLinkedinLine className="inline-block ml-2 mr-5 text-3xl" />
                   LinkedIn
                 </a>
                 <a
@@ -59,6 +73,7 @@ export default function Nav() {
                   className="navbar-link"
                   rel="noreferrer"
                 >
+                  <RiMailLine className="inline-block ml-2 mr-5 text-3xl" />
                   Contact
                 </a>
               </div>
