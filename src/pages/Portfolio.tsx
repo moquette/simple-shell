22db2f1faeb08a6bd9d0749021bcc0551b1f6536
Joaquin A. Moquette <joaquin@moquette.us>
2022-04-30 19:26:18 -0700

    chore: portfolio refacturing

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -2,36 +2,49 @@ import React from 'react'
 import {Layout} from '@/systems/core'
 import {ProjectCard as Project} from '@/systems/core'
 
+const portfolioItems = [
+  {
+    title: 'Base',
+    repo: 'https://github.com/moquette/base',
+    image: 'https://moquette.us/assets/base.791e9071.png',
+    description: 'A wicked FAST React boilerplate.',
+  },
+  {
+    title: 'Hello',
+    repo: 'https://github.com/moquette/hello',
+    image: 'https://moquette.us/assets/hello.88b39c5d.png',
+    description:
+      'Minimalistic cover page that makes a perfect GitHub user landing page, Single Page Application.',
+  },
+  {
+    title: 'RBSX',
+    repo: 'https://github.com/moquette/rbsx',
+    image: 'https://moquette.us/assets/rbsx.675cb04a.png',
+    description:
+      'A wicked fast technology stack for your next website or application.',
+  },
+  {
+    title: 'RTSX',
+    repo: 'https://github.com/moquette/rtsx',
+    image: 'https://moquette.us/assets/rtsx.d3da1b0b.png',
+    description:
+      'A wicked FAST Next.js stack for your next website or application.',
+  },
+]
+
 export default function Portfolio() {
   return (
     <Layout title="Portfolio">
       <div className="page portfolio pt-7">
         <h2>Portfolio</h2>
         <div className="grid gap-5 pt-2 md:grid-cols-2">
-          <Project title="Demo 1" url="#">
-            <p>
-              Morbi tristique senectus et netus et malesuada. Vel orci porta non
-              pulvinar neque.
-            </p>
-          </Project>
-          <Project title="Demo 2">
-            <p>
-              Morbi tristique senectus et netus et malesuada. Vel orci porta non
-              pulvinar neque.
-            </p>
-          </Project>
-          <Project title="Demo 3">
-            <p>
-              Morbi tristique senectus et netus et malesuada. Vel orci porta non
-              pulvinar neque.
-            </p>
-          </Project>
-          <Project title="Demo 4">
-            <p>
-              Morbi tristique senectus et netus et malesuada. Vel orci porta non
-              pulvinar neque.
-            </p>
-          </Project>
+          {portfolioItems.map(({title, repo, image, description}) => {
+            return (
+              <Project title={title} image={image} repo={repo} key={title}>
+                <p>{description}</p>
+              </Project>
+            )
+          })}
         </div>
       </div>
     </Layout>
--- src/systems/core/components/ProjectCard.tsx
+++ src/systems/core/components/ProjectCard.tsx
@@ -2,24 +2,21 @@ import React, {FC} from 'react'
 import {RiGithubLine} from 'react-icons/ri'
 interface Props {
   title: string
-  url?: string
+  repo?: string
+  image?: string
   children: JSX.Element
 }
 
-export const ProjectCard: FC<Props> = ({title, url, children}) => {
+export const ProjectCard: FC<Props> = ({title, repo, image, children}) => {
   return (
     <div className="rounded-lg border border-gray-200 shadow-md">
-      <img
-        className="rounded-t-lg"
-        src="https://picsum.photos/920/613"
-        alt=""
-      />
+      <img className="rounded-t-lg" src={image} alt="" />
       <div className="px-5 pt-5">
         <h5 className="mb-2 text-2xl font-bold tracking-tight">{title}</h5>
         {children}
         <p className="text-right">
           <a
-            href={url}
+            href={repo}
             target="_blank"
             rel="noreferrer"
             className="button full"
