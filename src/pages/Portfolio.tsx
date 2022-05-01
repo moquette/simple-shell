696664b2d4c1afeafaed5db3497340b2eee5c78c
Joaquin A. Moquette <joaquin@moquette.us>
2022-04-30 17:57:16 -0700

    chore: portfolio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -6,68 +6,108 @@ export default function Portfolio() {
     <Layout title="Portfolio">
       <div className="page portfolio pt-7">
         <h2>Portfolio</h2>
-        <p>
-          Sagittis purus sit amet volutpat consequat mauris nunc congue nisi.
-          Porttitor leo a diam sollicitudin tempor id eu nisl. Justo nec
-          ultrices dui sapien eget mi proin sed. Neque aliquam vestibulum morbi
-          blandit. Vitae tempus quam pellentesque nec nam. Diam volutpat commodo
-          sed egestas egestas fringilla phasellus faucibus scelerisque. Volutpat
-          est velit egestas dui id. Ut lectus arcu bibendum at varius vel.
-          Aliquam faucibus purus in massa tempor. Consectetur libero id faucibus
-          nisl tincidunt eget nullam non nisi. Feugiat vivamus at augue eget
-          arcu. Netus et malesuada fames ac turpis egestas sed. Egestas erat
-          imperdiet sed euismod nisi porta lorem. At auctor urna nunc id cursus
-          metus aliquam eleifend. Sit amet nisl purus in mollis nunc sed. Enim
-          eu turpis egestas pretium aenean pharetra magna ac placerat.
-        </p>
-        <p>
-          Risus nec feugiat in fermentum posuere urna. Pharetra pharetra massa
-          massa ultricies. Quis lectus nulla at volutpat diam ut venenatis
-          tellus. Eu augue ut lectus arcu. Egestas purus viverra accumsan in.
-          Lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi. Cras
-          sed felis eget velit aliquet. Sit amet justo donec enim diam vulputate
-          ut pharetra. Purus in massa tempor nec feugiat nisl. Arcu cursus
-          euismod quis viverra nibh cras.
-        </p>
-        <p>
-          Fusce ut placerat orci nulla pellentesque dignissim. Euismod lacinia
-          at quis risus sed vulputate odio. Morbi tristique senectus et netus et
-          malesuada. Vel orci porta non pulvinar neque. Viverra mauris in
-          aliquam sem fringilla ut morbi tincidunt. Odio morbi quis commodo
-          odio. Ultrices tincidunt arcu non sodales neque sodales. Montes
-          nascetur ridiculus mus mauris vitae. Sit amet venenatis urna cursus
-          eget nunc scelerisque viverra mauris. Tempus urna et pharetra pharetra
-          massa massa. Scelerisque felis imperdiet proin fermentum leo vel.
-          Habitasse platea dictumst vestibulum rhoncus est pellentesque.
-          Condimentum lacinia quis vel eros donec. Urna molestie at elementum eu
-          facilisis sed odio.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio
-          facilisis mauris sit amet massa vitae. Euismod lacinia at quis risus
-          sed vulputate odio. Rhoncus mattis rhoncus urna neque viverra justo
-          nec ultrices. At quis risus sed vulputate odio ut enim. Aliquam
-          ultrices sagittis orci a scelerisque purus semper. Ornare arcu dui
-          vivamus arcu felis bibendum ut. Semper eget duis at tellus at.
-          Tincidunt eget nullam non nisi est sit amet. Ornare arcu odio ut sem
-          nulla pharetra.
-        </p>
-        <p>
-          Accumsan tortor posuere ac ut consequat semper viverra nam. Dolor
-          magna eget est lorem ipsum dolor sit amet. Amet justo donec enim diam
-          vulputate ut. Eget aliquet nibh praesent tristique magna sit amet
-          purus. Amet cursus sit amet dictum sit amet justo donec enim. Elit
-          ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Nibh
-          praesent tristique magna sit. Ornare aenean euismod elementum nisi.
-          Egestas diam in arcu cursus euismod quis viverra nibh. Turpis egestas
-          integer eget aliquet nibh praesent tristique magna. Diam in arcu
-          cursus euismod quis viverra. Fermentum et sollicitudin ac orci.
-          Adipiscing tristique risus nec feugiat in fermentum posuere urna.
-          Imperdiet nulla malesuada pellentesque elit eget gravida. Adipiscing
-          commodo elit at imperdiet dui accumsan. Condimentum id venenatis a
-          condimentum vitae. Pharetra diam sit amet nisl.
-        </p>{' '}
+        <div className="grid gap-5 pt-2 md:grid-cols-2">
+          <div className="rounded-lg border border-gray-200 shadow-md">
+            <a href="#">
+              <img
+                className="rounded-t-lg"
+                src="https://picsum.photos/920/613"
+                alt=""
+              />
+            </a>
+            <div className="px-5 pt-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Demo 1
+                </h5>
+              </a>
+              <p>
+                Morbi tristique senectus et netus et malesuada. Vel orci porta
+                non pulvinar neque.
+              </p>
+              <p className="text-right">
+                <a href="#" className="button full">
+                  View on GitHub
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="rounded-lg border border-gray-200 shadow-md">
+            <a href="#">
+              <img
+                className="rounded-t-lg"
+                src="https://picsum.photos/920/613"
+                alt=""
+              />
+            </a>
+            <div className="px-5 pt-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Demo 2
+                </h5>
+              </a>
+              <p>
+                Morbi tristique senectus et netus et malesuada. Vel orci porta
+                non pulvinar neque.
+              </p>
+              <p className="text-right">
+                <a href="#" className="button full">
+                  View on GitHub
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="rounded-lg border border-gray-200 shadow-md">
+            <a href="#">
+              <img
+                className="rounded-t-lg"
+                src="https://picsum.photos/920/613"
+                alt=""
+              />
+            </a>
+            <div className="px-5 pt-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Demo 3
+                </h5>
+              </a>
+              <p>
+                Morbi tristique senectus et netus et malesuada. Vel orci porta
+                non pulvinar neque.
+              </p>
+              <p className="text-right">
+                <a href="#" className="button full">
+                  View on GitHub
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="rounded-lg border border-gray-200 shadow-md">
+            <a href="#">
+              <img
+                className="rounded-t-lg"
+                src="https://picsum.photos/920/613"
+                alt=""
+              />
+            </a>
+            <div className="px-5 pt-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Demo 4
+                </h5>
+              </a>
+              <p>
+                Morbi tristique senectus et netus et malesuada. Vel orci porta
+                non pulvinar neque.
+              </p>
+              <p className="text-right">
+                <a href="#" className="button full">
+                  View on GitHub
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   )
