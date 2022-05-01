3ba5d738e862c35cb392b38613903ac5d158a12d
Joaquin A. Moquette <joaquin@moquette.us>
2022-04-30 20:05:57 -0700

    chore: buttons

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -6,12 +6,14 @@ const portfolioItems = [
   {
     title: 'Base',
     repo: 'https://github.com/moquette/base',
+    demo: 'https://base.moquette.us',
     image: 'https://moquette.us/assets/base.791e9071.png',
     description: 'A wicked FAST React boilerplate.',
   },
   {
     title: 'Hello',
     repo: 'https://github.com/moquette/hello',
+    demo: 'https://hello.moquette.us',
     image: 'https://moquette.us/assets/hello.88b39c5d.png',
     description:
       'Minimalistic cover page that makes a perfect GitHub user landing page, Single Page Application.',
@@ -19,6 +21,7 @@ const portfolioItems = [
   {
     title: 'RBSX',
     repo: 'https://github.com/moquette/rbsx',
+    demo: 'https://rbsx.moquette.us',
     image: 'https://moquette.us/assets/rbsx.675cb04a.png',
     description:
       'A wicked fast technology stack for your next website or application.',
@@ -26,9 +29,33 @@ const portfolioItems = [
   {
     title: 'RTSX',
     repo: 'https://github.com/moquette/rtsx',
+    demo: 'https://rtsx.moquette.us',
     image: 'https://moquette.us/assets/rtsx.d3da1b0b.png',
     description:
       'A wicked FAST Next.js stack for your next website or application.',
+  },
+  {
+    title: 'Backpack',
+    repo: 'https://github.com/moquette/backpack',
+    demo: 'https://backpack.moquette.us',
+    image: 'https://moquette.us/assets/backpack.05544ee2.png',
+    description: 'A production ready webpack 5 boilerplate.',
+  },
+  {
+    title: 'Pull Up Plate',
+    repo: 'https://pullupplate.com',
+    demo: 'https://pullupplate.com',
+    image: 'https://moquette.us/assets/pullupplate.f8c33246.png',
+    description: 'Northern sacramento comfort food.',
+    wip: true,
+  },
+  {
+    title: 'AppKit',
+    repo: 'https://appkit.moquette.us/',
+    demo: 'https://appkit.moquette.us/',
+    image: 'https://moquette.us/assets/appkit.8eb4fc73.png',
+    description: 'Mobile UI kit.',
+    wip: true,
   },
 ]
 
@@ -38,9 +65,15 @@ export default function Portfolio() {
       <div className="page portfolio pt-7">
         <h2>Portfolio</h2>
         <div className="grid gap-5 pt-2 md:grid-cols-2">
-          {portfolioItems.map(({title, repo, image, description}) => {
+          {portfolioItems.map(({title, repo, demo, image, description}) => {
             return (
-              <Project title={title} image={image} repo={repo} key={title}>
+              <Project
+                title={title}
+                image={image}
+                repo={repo}
+                demo={demo}
+                key={title}
+              >
                 <p>{description}</p>
               </Project>
             )
--- src/systems/core/components/ProjectCard.tsx
+++ src/systems/core/components/ProjectCard.tsx
@@ -3,26 +3,43 @@ import {RiGithubLine} from 'react-icons/ri'
 interface Props {
   title: string
   repo?: string
+  demo?: string
   image?: string
   children: JSX.Element
 }
 
-export const ProjectCard: FC<Props> = ({title, repo, image, children}) => {
+export const ProjectCard: FC<Props> = ({
+  title,
+  repo,
+  demo,
+  image,
+  children,
+}) => {
   return (
     <div className="rounded-lg border border-gray-200 shadow-md">
-      <img className="rounded-t-lg" src={image} alt="" />
+      <a href={demo} target="_blank" rel="noreferrer">
+        <img className="rounded-t-lg" src={image} alt="" />
+      </a>
       <div className="px-5 pt-5">
         <h5 className="mb-2 text-2xl font-bold tracking-tight">{title}</h5>
         {children}
-        <p className="text-right">
+        <p className="grid grid-cols-2 gap-5 pt-2">
           <a
             href={repo}
             target="_blank"
             rel="noreferrer"
-            className="button full"
+            className="button text-center"
           >
-            Code Repository
-            <RiGithubLine className="ml-1 inline-block" />
+            Code
+          </a>
+
+          <a
+            href={demo}
+            target="_blank"
+            rel="noreferrer"
+            className="button text-center"
+          >
+            Demo
           </a>
         </p>
       </div>
